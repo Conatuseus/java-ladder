e5f932f9b02b0e5b553d376476b28d2c7edcdcb7
conatuseus <conatuseus@gmail.com>
2019-05-15 16:14:44 +0900

    플레이어 수보다 1작은 크기의 Line 생성하기

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -1,4 +1,21 @@
 package ladder.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Line {
+    private List<Boolean> points;
+
+    public Line(int countOfPlayer) {
+        this.points=this.pointsInit(countOfPlayer);
+    }
+
+    private List<Boolean> pointsInit(int countOfPlayer) {
+        return new ArrayList<>(Arrays.asList(new Boolean[countOfPlayer-1]));
+    }
+
+    public int size() {
+        return this.points.size();
+    }
 }
--- src/test/java/ladder/LineTest.java
+++ src/test/java/ladder/LineTest.java
@@ -1,10 +1,14 @@
 package ladder;
 
+import ladder.model.Line;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
+    @Test
+    void 플레이어수_보다_1작은_Line이_만들어지는지_확인(){
+        assertThat(new Line(5).size()).isEqualTo(4);
+    }
 
 }
