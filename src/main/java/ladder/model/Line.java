56a3e0e604f077907f9324027a8a05966f897fdd
conatuseus <conatuseus@gmail.com>
2019-05-15 17:08:18 +0900

    만들어진 Line 검증 구현

--- src/main/java/ladder/model/Line.java
+++ src/main/java/ladder/model/Line.java
@@ -1,21 +1,47 @@
 package ladder.model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Line {
-    private List<Boolean> points;
+    private List<Boolean> points = new ArrayList<>();
+    private static final Random RANDOM = new Random();
 
     public Line(int countOfPlayer) {
-        this.points=this.pointsInit(countOfPlayer);
+        this.pointsInit(countOfPlayer);
     }
 
-    private List<Boolean> pointsInit(int countOfPlayer) {
-        return new ArrayList<>(Arrays.asList(new Boolean[countOfPlayer-1]));
+    private void pointsInit(int countOfPlayer) {
+        boolean previous = false;
+        for (int i = 0; i < countOfPlayer - 1; i++) {
+            previous = nextPoint(previous);
+            points.add(previous);
+        }
+        this.checkPointsValid();
     }
 
-    public int size() {
+    private boolean nextPoint(boolean previous) {
+        if (!previous) {
+            return RANDOM.nextBoolean();
+        }
+        return false;
+    }
+
+    public int lineSize() {
         return this.points.size();
     }
+
+    public void checkPointsValid() {
+        for (int i = 1; i < this.lineSize(); i++) {
+            checkContinued(i - 1, i);
+        }
+    }
+
+    private void checkContinued(int left, int right) {
+        if (this.points.get(left) && this.points.get(right)) {
+            throw new IllegalArgumentException("이어지는 가로라인 발생");
+        }
+    }
+
 }
