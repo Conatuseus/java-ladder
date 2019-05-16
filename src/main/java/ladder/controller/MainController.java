6259bd8c5d97e642520ce7a570b99d735c2e1cdf
conatuseus <conatuseus@gmail.com>
2019-05-16 10:19:37 +0900

    중복검사 추가, 사다리 출력 기능 구현

--- src/main/java/ladder/controller/MainController.java
+++ src/main/java/ladder/controller/MainController.java
@@ -1,10 +1,18 @@
 package ladder.controller;
 
+import ladder.model.Ladder;
+import ladder.model.Player;
+import ladder.model.Players;
 import ladder.view.InputView;
 
 public class MainController {
     public static void main(String[] args) {
         String[] names = InputView.inputNames();
         int height = InputView.inputLadderHeight();
+
+        Players players=new Players(names);
+        Ladder ladder=new Ladder(height,names.length);
+        System.out.println(players.toString());
+        System.out.println(ladder.toString());
     }
 }
